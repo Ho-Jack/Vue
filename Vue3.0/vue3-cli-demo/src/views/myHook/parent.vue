96a2fae76edd39e0936030da0cbfcfeebddabd7d
Ho-Jack <965389230@qq.com>
2022-04-02 17:26:56 +0800

    1

--- Vue3.0/vue3-cli-demo/src/views/myHook/parent.vue
+++ Vue3.0/vue3-cli-demo/src/views/myHook/parent.vue
@@ -1,56 +1,26 @@
 
 <template>
-    <div style="display:flex;flex-direction:column;">
-        <p>
-            num1:
-            <input v-model.number="num1" />
-        </p>
-        <p>
-            num2:
-            <input v-model.number="num2" />
-        </p>
+    <div>
+        num1:<input v-model.number="num1" style="width:100px" />
+        <br />
+        num2:<input v-model.number="num2" style="width:100px" />
     </div>
-
     <span>加法等于:{{ addNum }}</span>
     <br />
     <span>减法等于:{{ subNum }}</span>
-
-    <p>{{ myscript }}</p>
 </template>
 
 <script setup>
-import { ref, watch, getCurrentInstance } from 'vue'
-const num1 = ref(1)
-const num2 = ref(2)
-//加法功能-Hook
-const useAdd = ({ num1, num2 }) => {
-    const addNum = ref(0)
-    watch([num1, num2], ([num1, num2], [prevNum1, prevNum2]) => {
-        add(num1, num2)
-    })
-    const add = (num1, num2) => {
-        console.log(num1, num2);
-        addNum.value = num1 + num2
-    }
-    return {
-        addNum,
-        add
-    }
-}
+import { ref } from 'vue'
+import useAdd from './useAdd.js'
+import { useSub } from './useSub.js'
+
+const num1 = ref(2)
+const num2 = ref(1)
+//加法功能-自定义Hook
 const { addNum, add } = useAdd({ num1, num2 })
 add(num1.value, num2.value)
-//减法功能-Hook
-// watch()
-const useSub = ({ num1, num2 }) => {
-    const subNum = ref(0)
-    const sub = () => {
-        subNum.value = num1 + num2
-    }
-    return {
-        addNum,
-        sub
-    }
-}
+//减法功能-自定义Hook
 const { subNum, sub } = useSub({ num1, num2 })
-
+sub(num1.value, num2.value)
 </script>
